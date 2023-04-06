ee5d4443058ef80519db47a773dfda9bcce35949
CodeSandbox <support@codesandbox.io>
2023-04-06 20:32:44 +0000

    "Initial commit"

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Abrar's Note Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
